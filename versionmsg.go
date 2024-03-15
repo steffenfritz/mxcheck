724b0e8474c045b64c086fbb7dfba9e5897492dc
Steffen / ampoff <steffenfritz@users.noreply.github.com>
2024-03-15 15:51:13 +0100

    Update versionmsg.go

--- versionmsg.go
+++ versionmsg.go
@@ -6,7 +6,7 @@ var Version string
 // Build is used by the Makefile to set the build, i.e. short git fingerprint
 var Build string
 
-var info = "mxcheck --  Copyright (C) 2019-2023  Steffen Fritz -- "
+var info = "mxcheck --  Copyright (C) 2019-2024  Steffen Fritz -- "
 
 var license = `This program comes with ABSOLUTELY NO WARRANTY.
 This is free software under GPL-3.0, and you are welcome to 
